8e382d265c515c01adfdbade3fca251de205cae9
waqa827 <waqarlilla00@gmail.com>
2025-08-06 18:41:41 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/yj7zp0.png' }, // Image URL from your request
+            image: { url: 'https://files.catbox.moe/p4yxcn.png' }, // Image URL from your request
             caption: `╭━━〔 *E3_HACKER-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
