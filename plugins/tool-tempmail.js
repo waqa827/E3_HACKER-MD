e1545c80546a417035e9786ae13969f68100a789
waqa827 <waqarlilla00@gmail.com>
2025-08-07 23:52:52 +0500

    Update tool-tempmail.js

--- plugins/tool-tempmail.js
+++ plugins/tool-tempmail.js
@@ -55,7 +55,7 @@ _Email will expire after 24 hours_
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363348739987203@newsletter',
+                        newsletterJid: '120363318478753709@newsletter',
                         newsletterName: 'TempMail Service',
                         serverMessageId: 101
                     }
