f57e1733d07921e3d12122da8aed4ef323b80b93
waqa827 <waqarlilla00@gmail.com>
2025-08-07 14:03:42 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, sender, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃',
+                    newsletterName: 'E3_HACKER_𝐌𝐃',
                     serverMessageId: 143
                 }
             }
