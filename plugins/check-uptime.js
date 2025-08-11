ee9839136cd95611008d57a77be4bcab6108b28f
waqa827 <waqarlilla00@gmail.com>
2025-08-11 16:19:47 +0500

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -119,8 +119,8 @@ ${config.DESCRIPTION}`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: config.OWNER_NAME || 'Arslan-MD Official',
+                    newsletterJid: '120363318478753709@newsletter',
+                    newsletterName: config.OWNER_NAME || 'E3_HACKER-MD Official',
                     serverMessageId: 143
                 }
             }
