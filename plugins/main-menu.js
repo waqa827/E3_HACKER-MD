c304821e2b58c12ae846e3b8e12c59f9e3c750ba
waqa827 <waqarlilla00@gmail.com>
2025-08-12 16:04:22 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -361,7 +361,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/ih3eko.mp3' },
+            audio: { url: 'https://files.catbox.moe/479tu4.mp3' },
             mimetype: 'audio/mp4',
             ptt: true,
             contextInfo: { 
